3e3af21dfa4c3e81029db4575778ebdf0faac5ee
illusionnext <fargan@st.amu.edu.pl>
2025-04-26 10:39:42 +0200

    Theme Switcher has been added

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className='grid place-content-center place-items-center font-bold text-2xl text-red-500'>
+      3D Developer Portfolio
+    </main>
+  );
+}
