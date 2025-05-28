4d3b1e8e42f8421ce90b95acb1030e2354092f98
illusionnext <fargan@st.amu.edu.pl>
2025-05-28 21:45:50 +0200

    added scale, position, rotation

--- components/hero-models/hero-experience.tsx
+++ components/hero-models/hero-experience.tsx
@@ -12,7 +12,7 @@ const HeroExperience = () => {
   return (
     <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
       <ambientLight intensity={0.2} color="#1a1a40" />
-      <directionalLight position={[5, 5, 5]} intensity={5} />
+      <directionalLight position={[5, 5, 5]} intensity={2} />
 
       <OrbitControls
         enablePan={false}
@@ -27,7 +27,13 @@ const HeroExperience = () => {
       {/*  <meshStandardMaterial color="tomato" />*/}
       {/*</mesh>*/}
 
-      <Room />
+      <group
+        scale={isMobile ? 0.2 : 1}
+        position={[0, -3.5, 0]}
+        rotation={[0, -Math.PI / 4, 0]}
+      >
+        <Room />
+      </group>
     </Canvas>
   );
 };
